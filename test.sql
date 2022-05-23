e374187c69ac0d76f40cac901a399cae50ef648f
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-05-23 14:45:15 -0500

    Datos de prueba insertados

--- test.sql
+++ test.sql
@@ -8,3 +8,8 @@ CREATE TABLE test1(
     label varchar(200) not null,
     PRIMARY KEY (idtest)
 );
+
+INSERT INTO `test1`(`label`) VALUES ('test 1');
+INSERT INTO `test1`(`label`) VALUES ('test 2');
+INSERT INTO `test1`(`label`) VALUES ('test 3');
+INSERT INTO `test1`(`label`) VALUES ('test 4');
